9d3fb93e51364cdb1cb00cb1f98cd6bb976e1c5c
Ashwin Purushotaman <ashwin.p88@gmail.com>
2024-11-29 12:48:38 -0500

    Add IOC

--- url-shortener.api/Program.cs
+++ url-shortener.api/Program.cs
@@ -1,3 +1,8 @@
+using url_shortener;
+using url_shortener.application;
+using url_shortener.core.interfaces;
+using url_shortener.infrastructure.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IRepository<string>, InMemoryRepository>();
+builder.Services.AddScoped<IApplication, Application>();
 
 var app = builder.Build();
 
