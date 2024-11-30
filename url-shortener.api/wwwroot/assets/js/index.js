0835b01e15c69964e638e9ac3664c036d5a9d729
Ashwin Purushotaman <ashwin.p88@gmail.com>
2024-11-30 11:33:06 -0500

    input validation

--- url-shortener.api/wwwroot/assets/js/index.js
+++ url-shortener.api/wwwroot/assets/js/index.js
@@ -1,2 +1,20 @@
+$(function() {
+    $("#domain").val(window.location.hostname);
+});
 
-$("#domain").val(window.location.hostname);
+
+function getRandomShortUrl(urlObj) {
+    $.ajax({
+        url: '/api/generate/generate',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(longUrl),
+        success: function(shortUrl) {
+            var fullShortUrl = window.location.origin + '/' + shortUrl;
+            $('#shorten-result').html('<p>Shortened URL: <a href="' + fullShortUrl + '">' + fullShortUrl + '</a></p>');
+        },
+        error: function(xhr, status, error) {
+            $('#shorten-result').html('<p class="text-danger">Error: ' + error + '</p>');
+        }
+    });
+}
