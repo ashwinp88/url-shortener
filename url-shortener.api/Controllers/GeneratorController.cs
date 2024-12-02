61dd0d8ec674f47fbe12b8d6c1c31acc3bc0bb95
Ashwin Purushotaman <ashwin.p88@gmail.com>
2024-12-01 22:30:48 -0500

    simplify api layer

--- url-shortener.api/Controllers/GeneratorController.cs
+++ url-shortener.api/Controllers/GeneratorController.cs
@@ -1,5 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
+using url_shortener.core;
+using url_shortener.core.Exceptions;
 using url_shortener.core.interfaces;
 using url_shortener.DTO;
 
@@ -17,15 +19,22 @@ namespace url_shortener
         }
 
         [HttpGet("{shortUrl}")]
-        public async Task<IActionResult> GetOne([Required]string shortUrl) {
-            if (!await app.ShortUrlExistsAsync(shortUrl)) {
-                return NotFound();
+        public async Task<IActionResult> GetOne([Required] string shortUrl)
+        {
+            try
+            {
+                string result = await app.GetFullUrlAsync(shortUrl);
+                return Ok(result);
             }
-            return Ok(await app.GetFullUrlAsync(shortUrl));
+            catch (UrlNotFoundException e)
+            {
+                return NotFound(e.Message);
+            }
         }
 
         [HttpPost("Generate")]
-        public async Task<IActionResult> GenerateShortUrl(UrlEntryDTO urlEntryDTO) {
+        public async Task<IActionResult> GenerateShortUrl(UrlEntryDTO urlEntryDTO)
+        {
             var shortUrl = await app.GenerateShortUrl(urlEntryDTO.Url);
             return CreatedAtAction(nameof(GetOne), new { shortUrl }, shortUrl);
         }
@@ -33,12 +42,15 @@ namespace url_shortener
         [HttpPost("GenerateCustom")]
         public async Task<IActionResult> GenerateCustomShortUrl(CustomUrlEntryDTO customUrlEntryDTO)
         {
-            if (await app.ShortUrlExistsAsync(customUrlEntryDTO.ShortUrl))
+            try
             {
-                return Conflict("This short url is taken.");
+                var shortUrl = await app.GenerateShortUrl(customUrlEntryDTO.Url, customUrlEntryDTO.ShortUrl);
+                return CreatedAtAction(nameof(GetOne), new { shortUrl }, shortUrl);
             }
-            var shortUrl = await app.GenerateShortUrl(customUrlEntryDTO.Url, customUrlEntryDTO.ShortUrl);
-            return CreatedAtAction(nameof(GetOne), new { shortUrl }, shortUrl);
+            catch (CustomUrlTakenException e)
+            {
+                return Conflict(e.Message);
+            }
         }
 
     }
